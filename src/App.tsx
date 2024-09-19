9a15c3fc0231ea899cfcd34be09cec27ffcabfc1
Karl Molina <karlgmolina@gmail.com>
2024-09-19 16:46:12 -0600

    Deploy to cloudflare

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import stormcastlelogo from "./assets/stormcastlelogo.svg";
 
 function App() {
